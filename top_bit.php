3d4c0673ffd46c4a9599f5eb48e3f4c26320138c
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2020-09-11 10:22:28 +1200

    changed name

--- top_bit.php
+++ top_bit.php
@@ -42,22 +42,3 @@
         <div class="box side">
             Side Bar goes here
         </div>
-        
-        <div class="box main">
-            <h2>First Page heading</h2>
-            
-            <p>
-                Sugar plum powder dessert bonbon powder lollipop lollipop muffin I love. Tiramisu dessert I love sugar plum apple pie marzipan chocolate bar brownie. Pudding tiramisu caramels biscuit tiramisu. I love gummies gummies pie sweet pudding I love I love. Donut halvah candy oat cake donut. Caramels wafer danish jelly-o.
-            </p>
-            
-           
-        </div>    <!-- / main -->
-        
-
-        <div class="box footer">
-            CC Stevens 2020
-        </div>    <!-- / footer -->
-    
-    </div>  <!-- / wrapper  -->
-    
-</body>        
