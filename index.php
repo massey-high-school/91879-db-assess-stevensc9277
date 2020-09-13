1c9a9efae1c4932bf64b59466cd876b00cdf2cde
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2020-09-14 09:50:29 +1200

    separated paragraphs

--- index.php
+++ index.php
@@ -8,9 +8,11 @@ include "topbit.php";
             <p>
 
 Food is one of life's simple pleasures. Please search the database by using the tools in the side bar. Hopefully you will find something that appeals.
-
+            </p>
+            <p>
 I must confess to having a dislike of vegetarian foods so many of the foods that I have reviewed are not that. The plan is to add more reviews to this site as time allows.
-
+            </p>
+            <p>
 If you'd like to suggest a food for review, please feel free to get in touch.
 
 ---
