202948010d7a095af6a5c8616047cfd75de3992f
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2020-09-18 10:46:46 +1200

    added submit button

--- topbit.php
+++ topbit.php
@@ -58,5 +58,6 @@
         <div class="box side">
             <h2>Search | <a class="side"href="show_all.php">Show All</a></h2>
             <form method="post" action="food_search.php" type="text" name="food" size="40" value required placeholder="Food search..."> 
-            <input class="submit" name="find_food" value=""></form>
+            <input class="submit" name="find_food" value="">
+            <input type="submit" value="Submit"></form>
         </div>
